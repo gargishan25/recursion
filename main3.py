817520071fe6f5008167ba66c45840d46b957a09
fullycharged25 <gargishan2010@gmail.com>
2026-06-30 19:32:18 -0700

    Create main3.py

---
+++ main3.py
@@ -0,0 +1,17 @@
+#Given a list of numbers, find the largest number in that list recursively.
+nums = [1,2,5,4,7,2,3,10,12,15]
+def largest(list,i):
+    if i == (len(list)-1):
+        return list[i]
+    biggest = largest(list,i+1)
+    if list[i] > biggest:
+        return list[i]
+    else:
+        return biggest
+    # if biggest<list[i]:
+    #     biggest = list[i]
+    # i+=1
+    # print(biggest)
+    # largest(list,i,biggest)
+    # return biggest
+print(largest(nums,0))
