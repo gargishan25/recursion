9a194a153801c188b580febc8cd869294ded472e
fullycharged25 <gargishan2010@gmail.com>
2026-07-11 22:32:19 -0700

    Create main5.py

---
+++ main5.py
@@ -0,0 +1,21 @@
+a = 0
+b = 0
+c = 0
+inp = "([)]"
+for i in inp:
+    if i == "{":
+        a +=1
+    elif i == "(":
+        b+=1
+    elif i == "[":
+        c+=1
+    if i == "}":
+        a -=1
+    elif i == ")":
+        b-=1
+    elif i == "]":
+        c-=1
+if a == 0 and b == 0 and c == 0:
+    print("These are balanced parentheses.")
+else:
+    print("These are not balanced parentheses.")
